1cf8d4e8e8574af8b01de1112216401bbf2ac8f2
Ruiminmrm <maruimin1998@gmail.com>
2023-10-17 16:27:38 +0200

    rl_3 done

--- RL/rl3/student_3_2_1.py
+++ RL/rl3/student_3_2_1.py
@@ -3,16 +3,16 @@
 rewards = [10, -10, -10, 10, -10, 10, -10, 10, -10, 10, -10, 10, -10]
 
 # Q learning learning rate
-alpha = 0
+alpha = 0.00000001
 
 # Q learning discount rate
-gamma = 0
+gamma = 0.2 
 
 # Epsilon initial
 epsilon_initial = 1
 
 # Epsilon final
-epsilon_final = 1
+epsilon_final = 0.2
 
 # Annealing timesteps
 annealing_timesteps = 1
--- RL/rl3/student_3_2_2.py
+++ RL/rl3/student_3_2_2.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # rewards: [golden_fish, jellyfish_1, jellyfish_2, ... , step]
-rewards = [-10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10]
+rewards = [100, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -10, -1]
+
 
 # Q learning learning rate
-alpha = 0
+alpha = 0.4
 
 # Q learning discount rate
-gamma = 0
+gamma = 0.8
 
 # Epsilon initial
 epsilon_initial = 1
